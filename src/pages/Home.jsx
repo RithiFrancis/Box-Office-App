32f6ddbfcb741b011b24e6895c0751de090139e0
Florance Rithi <florancerithi25@gmail.com>
2024-01-11 19:09:29 +0530

    displayed detailed show data on the show page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -22,35 +22,35 @@ const Home = () => {
 
   const onSearch = async ({ q, searchOption }) => {
     setFilter({ q, searchOption });
-
-    const renderApiData = () => {
-      if (apiDataError) {
-        return <div>Error occured : {apiDataError.message}</div>;
-      }
-
-      if (apiData?.length === 0) {
-        return <div>No Results</div>;
-      }
-
-      if (apiData) {
-        return apiData[0].show ? (
-          <ShowGrid shows={apiData} />
-        ) : (
-          <ActorGrid actors={apiData} />
-        );
-      }
-
-      return null;
-    };
-
-    return (
-      <div>
-        <SearchForm onSearch={onSearch} />
-
-        <div>{renderApiData()}</div>
-      </div>
-    );
   };
+
+  const renderApiData = () => {
+    if (apiDataError) {
+      return <div>Error occured : {apiDataError.message}</div>;
+    }
+
+    if (apiData?.length === 0) {
+      return <div>No Results</div>;
+    }
+
+    if (apiData) {
+      return apiData[0].show ? (
+        <ShowGrid shows={apiData} />
+      ) : (
+        <ActorGrid actors={apiData} />
+      );
+    }
+
+    return null;
+  };
+
+  return (
+    <div>
+      <SearchForm onSearch={onSearch} />
+
+      <div>{renderApiData()}</div>
+    </div>
+  );
 };
 
 export default Home;
